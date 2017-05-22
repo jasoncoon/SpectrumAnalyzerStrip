9fa4516213e4de7086fe7cbfa000329514d95e9b
Jason Coon <jasoncoon@users.noreply.github.com>
2017-05-22 15:21:11 -0500

    Update SpectrumAnalyzerStrip.ino

--- SpectrumAnalyzerStrip.ino
+++ SpectrumAnalyzerStrip.ino
@@ -1,13 +1,13 @@
 /*
-* Based on SpectrumAnalyzerBasic by Paul Stoffregen included in the Teensy Audio Library
-* Modified by Jason Coon for FastLED
-* Requires Teensyduino 1.20 and the Teensy Audio Library
-* Also requires FastLED 2.1
-*
-* You can change the pin used for ADC with the ADC_INPUT_PIN definition below.
-* There are no dedicated ADC pins brought out on the SmartMatrix Shield,
-* but even if you've used all the pins on the SmartMatrix expansion header,
-* you can use solder pins directly to the Teensy to use A14/DAC, A11, or A10
+  Based on SpectrumAnalyzerBasic by Paul Stoffregen included in the Teensy Audio Library
+  Modified by Jason Coon for FastLED
+  Requires Teensyduino 1.20 and the Teensy Audio Library
+  Also requires FastLED 2.1
+
+  You can change the pin used for ADC with the ADC_INPUT_PIN definition below.
+  There are no dedicated ADC pins brought out on the SmartMatrix Shield,
+  but even if you've used all the pins on the SmartMatrix expansion header,
+  you can use solder pins directly to the Teensy to use A14/DAC, A11, or A10
 */
 
 // all these libraries are required for the Teensy Audio Library
@@ -17,13 +17,11 @@
 #include <SD.h>
 
 #include <FastLED.h>
-#include <IRremote.h>
 
 #define ADC_INPUT_PIN   A2
 
 #define LED_PIN     2
 #define CLOCK_PIN   3
-#define IR_RECV_PIN 12
 #define COLOR_ORDER GRB
 #define CHIPSET     APA102
 #define NUM_LEDS    63
@@ -46,70 +44,49 @@ int shown;
 uint8_t brightness = 255;
 
 CRGB leds[NUM_LEDS];
-IRrecv irReceiver(IR_RECV_PIN);
 
 CRGBPalette16 gPalette = RainbowColors_p;
 
 void setup()
 {
-    //delay(3000); // sanity delay
-    //Serial.println("ready...");
+  FastLED.addLeds<CHIPSET, LED_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
+  FastLED.setBrightness(brightness);
 
-    FastLED.addLeds<CHIPSET, LED_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
-    FastLED.setBrightness(brightness);
-
-    // Initialize the IR receiver
-    irReceiver.enableIRIn();
-    irReceiver.blink13(true);
-
-    // Audio requires memory to work.
-    AudioMemory(12);
+  // Audio requires memory to work.
+  AudioMemory(12);
 }
 
 void loop()
 {
-    unsigned int requestedDelay = spectrum();
+  spectrum();
 
-    FastLED.show(); // display this frame
-
-    // handleInput(requestedDelay);
+  FastLED.show();
 }
 
-unsigned int spectrum() {
-    if (fft.available()) {
-        // read from the FFT
+void spectrum() {
+  if (fft.available()) {
+    // read from the FFT
 
-        // I'm skipping the first two bins, as they seem to be unusable
-        // they start out at zero, but then climb and don't come back down
-        // even after sound input stops
-        level = fft.read(2, 127);
-    }
+    // I'm skipping the first two bins, as they seem to be unusable
+    // they start out at zero, but then climb and don't come back down
+    // even after sound input stops
+    level = fft.read(2, 127);
+  }
 
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
 
-    int val = level * scale;
-    if (val >= NUM_LEDS) val = NUM_LEDS - 1;
+  int val = level * scale;
+  if (val >= NUM_LEDS) val = NUM_LEDS - 1;
 
-    if (val >= shown) {
-        shown = val;
-    }
-    else {
-        if (shown > 0) shown = shown - 1;
-        val = shown;
-    }
+  if (val >= shown) {
+    shown = val;
+  }
+  else {
+    if (shown > 0) shown = shown - 1;
+    val = shown;
+  }
 
-    for (int i = 0; i < shown; i++) {
-        leds[i] = ColorFromPalette(gPalette, i * 4, 255, NOBLEND);
-    }
-
-    return 0;
-}
-
-void handleInput(unsigned int requestedDelay) {
-    unsigned int requestedDelayTimeout = millis() + requestedDelay;
-
-    while (true) {
-        if (millis() >= requestedDelayTimeout)
-            break;
-    }
+  for (int i = 0; i < shown; i++) {
+    leds[i] = ColorFromPalette(gPalette, i * 4, 255, NOBLEND);
+  }
 }
